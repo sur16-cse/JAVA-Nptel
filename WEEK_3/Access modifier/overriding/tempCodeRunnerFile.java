0f57624a987b6ee72e9a991491e4f3145505208f
surbhi kumari <surbhikumaridav@gmail.com>
2021-10-15 10:01:24 +0530

    update 1

---
+++ WEEK_3/Access modifier/overriding/tempCodeRunnerFile.java
@@ -0,0 +1,2 @@
+Point2D cast=(Point2D) p2;
+        // cast.display();
---
+++ WEEK_3/Access modifier/overriding/using_cast.java
@@ -0,0 +1,31 @@
+class Point2D{
+    int x;int y;
+    Point2D(int a,int b){
+        x=a;
+        y=b;
+    }
+    void display(){
+        System.out.println("x="+x+" "+"y="+y);
+    }
+}
+
+class Point3D extends Point2D{
+    int z;
+    Point3D(int c){
+        z=c;
+    }
+    void display(){
+        System.out.println("x="+x+" "+"y="+y+" "+"z="+z);
+    }
+}
+
+class using_cast{
+     public static void main(String args[]) {
+        Point2D p1=new Point2D(3,-4);
+        p1.display();
+        Point3D p2=new Point3D(9,2,5);
+        p2.display();
+        Point2D cast=(Point2D) p2;
+        cast.display();
+    }
+}
